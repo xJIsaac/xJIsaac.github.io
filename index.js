69f3698f3e9be610061c6c403558d75444de76d7
xJIsaac <xjisaac@gmail.com>
2021-03-03 22:43:57 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const cards = [
+    {
+        link: "https://vizil-jaime-moreno.herokuapp.com/",
+        title: "Vizil",
+        img: "./images/vizil.png",
+        info: "Vizil is a simplistic quote keeping application build on Ruby on Rails. The app uses PostgreSQL to store quotes. Adding a quote is done using the <i>Simple Form</i> gem which appears in a modal. Have fun adding a new quote."
+    },
+    {
+        link: "https://nomster-jaime-moreno.herokuapp.com/",
+        title: "Nomster",
+        img: "./images/nomster.png",
+        info: "Nomster is a Yelp clone build on Ruby on Rails. Users can create accounts, upload images, create a profile for a business, rate a business and comment on a business profile. The app also include the Google Maps API to display locations of each business. With the use of AWS S3 the application can store user's images."
+    },
+    {
+        link: "https://cognitio-jaime-moreno.herokuapp.com/courses#",
+        title: "Cognitio",
+        img: "./images/cognitio.png",
+        info: "Cognitio is a Udemy clone build on Ruby on Rails. User's can create accounts, upload images and videos, create courses, view courses and enroll in courses. The application even supports payment transactions via the <i>Stripe</i> API."
+    },
+    {
+        link: "https://thecheckmates-chessapp.herokuapp.com/",
+        title: "Chesster",
+        img: "./images/chesster.png",
+        info: "At the moment this application is still in development. Chesster is a Chess game application which incorporates all the rules of chess. The app is build on Ruby on Rails. The board features drag-n-drop for moving pieces. Players can create accounts and invite friends to play."
+    },
+    {
+        link: "https://empires-battle-app.herokuapp.com/",
+        title: "Empires",
+        img: "./images/empires.png",
+        info: "Empires is a battle simulation app which is build on Ruby on Rails. The application uses a simple algorithm which includes a bit chance to determine the winner of two armies. Anyone can create a new army and battle it against another."
+    }
+]
+
+function render(item) {
+    const { title, link, img, info } = item
+    return `
+    <!-- ${title} -->
+    <li>
+        <div>
+            <a href=${link}>
+                <h3>${title}</h3>
+                <img src=${img}>
+            </a>
+            <p>${info}</p>
+        </div>	
+    </li>
+    `
+}
+
+function loadPortfolio() {
+    const portfolioList = document.querySelector("#portfolio > ul")
+    cards.forEach(card => {
+        portfolioList.innerHTML += render(card)
+    });
+}
+  
+window.addEventListener("DOMContentLoaded", loadPortfolio);
