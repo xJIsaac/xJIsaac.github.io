a2ab0dc91dad4f77d29158c4f8027f3d04430062
xJIsaac <xjisaac@gmail.com>
2021-06-24 19:38:48 -0500

    Portfolio cards redesigned

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ function render({ title, link, repoLink, img, info }) {
   return `
     <!-- ${title} -->
     <div class="container column">
-      <div class="space-between v-center no-margin">
+      <div class="v-center no-margin">
         <a href="${link}">
           <h3>${title}</h3>
         </a>
-        <a href="${repoLink}">
-          Repo
-        </a>
+        <div class="v-center">
+          <a href="${repoLink}">
+            <img src="./images/github.png" alt="github" id="github-repo-img" />
+          </a>
+        </div>
       </div>
       <a href="${link}">
         <img src="${img}" />
       </a>
       <p>${info}</p>
     </div>
+
   `;
 }
 
