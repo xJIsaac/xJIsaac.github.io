af898a123607b069b47f88a93128dbe781b1f3c5
xJIsaac <xjisaac@gmail.com>
2023-06-29 23:19:50 -0500

    Improved the heading

---
+++ Components/header/animateText.js
@@ -0,0 +1,4 @@
+window.addEventListener("load", function () {
+  var animatedText = document.getElementById("animated-text");
+  animatedText.style.opacity = "1";
+});
--- loadSections.js
+++ loadSections.js
@@ -6,5 +6,19 @@ function loadProfile() {
     });
 }
 
+function loadHeader() {
+  fetch("./Components/header/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header").innerHTML = data;
+    });
+}
+
 // Call the functions to load the header and footer
+loadHeader();
 loadProfile();
+
+window.addEventListener("load", function () {
+  // Scroll to the top of the page
+  window.scrollTo(0, 0);
+});
